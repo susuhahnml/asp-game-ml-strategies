80848aa64030e84152b314f3a576286ecd2180a0
Lukas Seiling <seiling@campus.tu-berlin.de>
2020-04-15 20:14:45 +0200

    added regularizers

--- src/approaches/alpha_zero/net_alpha.py
+++ src/approaches/alpha_zero/net_alpha.py
@@ -1,4 +1,3 @@
-        
 import os.path
 import csv
 from py_utils.logger import log
@@ -20,6 +19,7 @@ from structures.net import Net
 from tensorflow.keras.layers import Input
 from tensorflow.keras.optimizers import Adam
 import tensorflow.keras.backend as K
+from tensorflow.keras.regularizers import l2
 
 def cross_entropy(y_true, y_pred):
     return tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred)
@@ -38,12 +38,12 @@ class NetAlpha(Net):
         arch = self.args.architecture_name
         if arch=="default":
             inputs = Input(shape=(state_size,))
-            hidden1 = Dense(120, activation='relu')(inputs)
-            hidden2 = Dense(30, activation='relu')(hidden1)
-            hidden3 = Dense(120, activation='relu')(hidden2)
+            hidden1 = Dense(120, activation='relu', activity_regularizer=l2(0.0001))(inputs)
+            hidden2 = Dense(30, activation='relu', activity_regularizer=l2(0.0001))(hidden1)
+            hidden3 = Dense(120, activation='relu', activity_regularizer=l2(0.0001))(hidden2)
             hidden4 = BatchNormalization()(hidden3)
-            pi = Dense(action_size, activation='linear', name='pi_non_softmaxed')(hidden4) 
-            v = Dense(1, activation='tanh', name='v')(hidden4)         
+            pi = Dense(action_size, activation='relu', activity_regularizer=l2(0.0001), name='pi_non_softmaxed')(hidden4) 
+            v = Dense(1, activation='tanh', activity_regularizer=l2(0.0001), name='v')(hidden4)         
             model = Model(inputs=inputs, outputs=[pi,v])
             self.model = model
             self.compile_model(self.model)
